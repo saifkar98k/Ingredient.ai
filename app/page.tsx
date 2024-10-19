fdb8da558108b7c50a7548157bff98790fe6f336
saifkar98k <saif.231824.ci@mhssce.ac.in>
2024-10-20 01:14:34 +0530

    Added component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import Image from 'next/image';
+import FoodReviewer from '@/components/FoodReviewer';
 
 export default function Home() {
-  return (
+  return (<>
+      <FoodReviewer/>
+
+  <div className=' p-4 text-center bg-black'>
+<h6 className='text-xs mb-2 text-green-500'>Rank 3 === Healthy</h6>
+<h6 className='text-xs mb-2 text-yellow-500'>Rank 2 === Caution</h6>
+<h6 className='text-xs mb-2 text-red-500'>Rank 1 === Harmful</h6>
+</div>
 <h1>Hello World</h1>
-  );
+</>
+);
 }
